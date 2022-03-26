e7a90a4c517d433b91503e3cfefc554c25e8792f
Widdy LOUIS <louisws@outlook.fr>
2022-03-26 22:52:03 +0100

    Update index.js

--- #1/index.js
+++ #1/index.js
@@ -1,18 +1,23 @@
 const cookiesBox = document.querySelector('.wrapper'),
-      acceptBtn = cookiesBox.querySelector('.buttons button');
+      acceptBtn = cookiesBox.querySelector('button');
 
 acceptBtn.onclick = () => {
   // réglage du cookie pendant 1 mois, après 1 mois, le cookie expirera automatiquement
-  document.cookie = "CooKieBy=Codwerk; max-age="+60*60*24*30;
+  let date = new Date();
+  date.setTime(date.getTime() + (60 * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = "CooKieBy=Codwerk;" + expires +"; path=/; SameSite=None; Secure";
   
   if(document.cookie) { // si le cookie ci-dessus est défini
-    cookiesBox.classList.add('hide'); // masquer la boîte à cookies une fois le cookie défini
+    // masquer la boîte à cookies une fois le cookie défini
+    cookiesBox.classList.add('hide'); 
   } else {
-    alert(`Le cookie ne peut pas être défini !`); // si le cookie ne peut pas être défini, l'alerte est une erreur
+    // si le cookie ne peut pas être défini, l'alerte est une erreur
+    alert(`Le cookie ne peut pas être défini !`); 
   }
 
   // permet de masquer la boîte à cookies si le cookie est défini et n'a pas expiré
-  let checkCookie = document.cookie.indexOf("CooKieBy=Codwerk"); // vérification de notre cookie défini
+  let checkCookie = document.cookie.indexOf("CooKieBy"); // vérification de notre cookie défini
   // si le cookie est défini, masquez la boîte à cookies, sinon affichez-le
-  checkCookie != 1 ? cookiesBox.classList.add('hide') : checkCookie.classList.remove('hide');
+  checkCookie != -1 ? cookiesBox.classList.add('hide') : cookiesBox.classList.remove('hide');
 }
